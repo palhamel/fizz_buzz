b7d9fff754f3a777315882db299af88a79d5853f
palhamel <pal.hamel@gmail.com>
2019-10-31 13:51:12 +0100

    Code to handle wrong user input

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,11 @@
 # This is our code to test:
 def fizz_buzz(number)
+
+    until number.is_a?(Fixnum) do
+      print "Please enter a number: "
+      number = Integer(gets) rescue nil
+    end
+
     if number % 5 == 0 and number % 3 == 0
       'fizzbuzz'
     elsif number % 3 == 0
@@ -8,16 +14,49 @@ def fizz_buzz(number)
       'buzz'
     elsif number % 4 == 0
       'ZAP!'
-    elsif number % 17 == 17
-      'Raggadish'
     else
       number
     end 
 end
 
+=begin
+  
+call:
+> load './lib/fizz_buzz.rb'
+
+> fizz_buzz(10)
+
+> fizz_buzz(3)
+
+> fizz_buzz(4)
+
+> fizz_buzz(13879)
+
+> fizz_buzz('banana')
+
+  
+=end
 
 
 
+
+#  This code worked great:
+
+# def fizz_buzz(number)
+#   if number % 5 == 0 and number % 3 == 0
+#     'fizzbuzz'
+#   elsif number % 3 == 0
+#     'fizz'
+#   elsif number % 5 == 0
+#     'buzz'
+#   elsif number % 4 == 0
+#     'ZAP!'
+#   else
+#     number
+#   end 
+# end
+
+#  First try:
 
 # def fizz_buzz(number)
 #   if number % 3 == 0
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -23,8 +23,8 @@ describe 'fizz_buzz' do
         expect(fizz_buzz(4)).to eq 'ZAP!'
     end
 
-    it "returns 'Raggadish' if number is 17" do
-        expect(fizz_buzz(17)).to eq 17
+    it "Please enter a number:" do
+        expect(fizz_buzz(4)).to eq 'ZAP!'
     end
 
 end
@@ -32,3 +32,42 @@ end
 
 
 
+
+
+
+
+
+#  This code worked great:
+
+=begin
+    
+rescue => exception
+    
+end
+require './lib/fizz_buzz'
+
+describe 'fizz_buzz' do
+
+    it 'returns 1 if number is 1' do
+        expect(fizz_buzz(1)).to eq 1 
+    end
+
+    it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(3)).to eq 'fizz'
+    end
+
+    it "returns 'buzz' if number is divisible by 5" do
+        expect(fizz_buzz(5)).to eq 'buzz'
+    end
+
+    it "returns 'fizzbuzz' if number is divisible by 15" do
+        expect(fizz_buzz(15)).to eq 'fizzbuzz'
+    end
+
+    it "returns 'ZAP!' if number is divisible by 4" do
+        expect(fizz_buzz(4)).to eq 'ZAP!'
+    end
+
+end
+
+=end
