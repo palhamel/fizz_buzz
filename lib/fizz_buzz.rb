2458cb3af7d4e54e771300674049354aecccfedf
palhamel <pal.hamel@gmail.com>
2019-10-31 10:31:23 +0100

    First file to pass completet test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,29 @@
 # This is our code to test:
 def fizz_buzz(number)
-    if number % 3 == 0
+    if number % 5 == 0 and number % 3 == 0
+      'fizzbuzz'
+    elsif number % 3 == 0
       'fizz'
+    elsif number % 5 == 0
+      'buzz'
     else
       number
     end 
 end
+
+
+
+
+
+# def fizz_buzz(number)
+#   if number % 3 == 0
+#     'fizz'
+#   else
+#     number
+#   end 
+# end
+
+
 
 # def fizz_buzz(number)
 #   if number % 5 == 0
